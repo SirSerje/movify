ad520f0f804bacc5544e007c440b5e26a2f8a5ae
SirSerje <sir.serje@gmail.com>
2019-02-24 22:04:36 +0200

    fix NewPost.js

--- src/Components/Post/NewPost.js
+++ src/Components/Post/NewPost.js
@@ -3,46 +3,46 @@ import {connect} from "react-redux";
 import {createPost} from '../../actions/postAction'
 
 class NewPost extends Component {
-    state = {
-        title: "",
-        body: "",
-    };
+  state = {
+    title: "",
+    body: "",
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        if (this.state.title.trim() && this.state.body.trim()) {
-            this.props.onAddPost(this.state);
-            this.handleReset();
-        }
-    };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.title.trim() && this.state.body.trim()) {
+      this.props.onAddPost(this.state);
+      this.handleReset();
+    }
+  };
 
-    handleInputChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    };
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  };
 
-    handleReset = () => {
-        this.setState({
-            title: "",
-            body: "",
-        })
-    };
+  handleReset = () => {
+    this.setState({
+      title: "",
+      body: "",
+    })
+  };
 
-    render() {
-        return (
-            <div>
-                <form onChange={this.handleSubmit}>
-                    <div>
-                        <input type="text"
-                               placeholder="Title"
-                               className="form-control"
-                               name="title"
-                               onChange={this.handleInputChange}
-                               value={this.state.title}
-                        />
-                    </div>
-                    <div>
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <input type="text"
+                   placeholder="Title"
+                   className="form-control"
+                   name="title"
+                   onChange={this.handleInputChange}
+                   value={this.state.title}
+            />
+          </div>
+          <div>
                         <textarea name="body"
                                   id=""
                                   cols="20"
@@ -52,26 +52,29 @@ class NewPost extends Component {
                                   onChange={this.handleInputChange}
                                   value={this.state.body}>
                         </textarea>
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-primary">Add Post</button>
-                        <button type="button"
-                                className="btn btn-warning"
-                                onChange={this.handleReset}>Reset
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+          </div>
+          <div>
+            <button onClick={this.handleSubmit}
+                    type="submit"
+                    className="btn btn-primary">Add Post
+            </button>
+            <button type="button"
+                    className="btn btn-warning"
+                    onClick={this.handleReset}>Reset
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-    onAddPost: post => {
-        dispatch(createPost(post))
-    }
+  onAddPost: post => {
+    dispatch(createPost(post))
+  }
 });
 
 export default connect(
-    null,
-    mapDispatchToProps)(NewPost);
+  null,
+  mapDispatchToProps)(NewPost);
